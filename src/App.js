9ca210c08d816b2e844686d3d754c4bbb36e3bcd
ntting <ttt123n2@outlook.com>
2024-10-05 16:32:36 +0800

    ntting的blog-v1.1

--- src/App.js
+++ src/App.js
@@ -2,9 +2,7 @@ import './App.css';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Suspense, lazy } from 'react';
 
-const Comic = lazy(() => import('@/containers/Comic/index'));
 const Floating = lazy(() => import('@/containers/Floating/index'));
-const Scroll = lazy(() => import('@/containers/Scroll/index'));
 
 
 function App() {
@@ -13,9 +11,7 @@ function App() {
       <Router>
         <Suspense fallback={<div className='loading_page'>Loading...</div>}>
         <Routes>
-          <Route element={ <Comic /> } path="/comic" />
           <Route element={ <Floating /> } path="/" />
-          <Route element={ <Scroll /> } path="/scroll" />
         </Routes>
         </Suspense>
       </Router>
